6fc0c97fc626f7896eea90c0bc808254b260424a
Atanas Delevski <adelevski@gmail.com>
2022-12-12 17:04:46 -0600

    added day 2 data renamed day 1 data

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -2,10 +2,6 @@ import scala.io.Source
 
 @main def main() = {
     val elvesString: Array[String] = Source.fromFile("data.txt").mkString.split("\n\n")
+    
 
-    val elves: Array[Array[Int]] = elves.map { x => x.split("\n").map(_.toInt) }
-
-    println(elves(0)(0))
-
-    // println(lines)
 }
