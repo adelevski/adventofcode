19bff2ade3d448a560591e990f3d620fc87ea99a
Atanas Delevski <adelevski@gmail.com>
2022-12-14 00:12:10 -0600

    finished day 2 part two scala

--- src/main/scala/day2.scala
+++ src/main/scala/day2.scala
@@ -1,36 +1,50 @@
 import scala.io.Source
 
 
-var fix = Map('A' -> 'X', 'B' -> 'Y', 'C' -> 'Z')
+var part_one_char_map = Map('A' -> 'X', 'B' -> 'Y', 'C' -> 'Z')
+var part_one_base_map = Map('X' -> 1, 'Y' -> 2, 'Z' -> 3)
+var part_two_outcome_map = Map('X' ->  0, 'Y' -> 3, 'Z' -> 6)
+var part_two_win_base_map = Map('A' -> 2, 'B' -> 3, 'C' -> 1)
+var part_two_lose_base_map = Map('A' -> 3, 'B' -> 1, 'C' -> 2)
+var part_two_draw_base_map = Map('A' -> 1, 'B' -> 2, 'C' -> 3)
+
 def scoreOne(x: String): Int = {
+    // Init
     var score: Int = 0
 
-    // draw
-    if (fix.get(x(0)).get == x(2)) score += 3
+    // Draw
+    if (part_one_char_map.get(x(0)).get == x(2)) score += 3
 
-    // win
-    if (x(0) == 'A' && x(2) == 'Y') score += 6
-    if (x(0) == 'B' && x(2) == 'Z') score += 6
-    if (x(0) == 'C' && x(2) == 'X') score += 6
+    // Win
+    if (x(0) == 'A' && x(2) == 'Y') score += 6 // Rock
+    if (x(0) == 'B' && x(2) == 'Z') score += 6 // Paper
+    if (x(0) == 'C' && x(2) == 'X') score += 6 // Scissors
 
-    // base
-    score += Map('X' -> 1, 'Y' -> 2, 'Z' -> 3).get(x(2)).get
+    // Base
+    score += part_one_base_map.get(x(2)).get
+
+    // Return
     score
 }
 
 def scoreTwo(x: String): Int = {
+    // Init
     var score: Int = 0
 
-    // draw
+    // Win, Lose, Draw
+    score += part_two_outcome_map.get(x(2)).get
 
-    // win
+    // Base
+    if (x(2) == 'Z') score += part_two_win_base_map.get(x(0)).get
+    if (x(2) == 'X') score += part_two_lose_base_map.get(x(0)).get
+    if (x(2) == 'Y') score += part_two_draw_base_map.get(x(0)).get
 
-    //base
+    // Return 
     score
 }
 
 @main def main() = {
     val lines: List[String] = Source.fromFile("day2.txt").getLines.toList
-    println(lines.map(scoreOne).sum)
-    println(lines.map(scoreTwo).sum)
+    println("Part 1: " + lines.map(scoreOne).sum)
+    println("Part 2: " + lines.map(scoreTwo).sum)
 }
