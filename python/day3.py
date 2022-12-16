e3767bad080f7bfbba25ce8db131369f31cbfb91
Atanas Delevski <adelevski@gmail.com>
2022-12-15 21:33:38 -0600

    almost done

--- python/day3.py
+++ python/day3.py
@@ -5,22 +5,22 @@ Advent of Code 2022 - Day 3 (Parts I & II)
 """
 
 
-
-
 def score_asci(x):
     if 65 <= x <= 90:
-        return (x-35)
+        return (x-38)
     else:
         return (x-96)
 
 def part_one(x):
     first, second = x[:len(x)//2], x[len(x)//2:]
     letter_asci = ord([x for x in first if x in second][0])
-    print(letter_asci)
     return score_asci(letter_asci)
     
-def part_two(x):
-    pass
+def part_two(lines):
+    part_two_score = 0
+    for i in range(0, len(lines), 3):
+        part_two_score += score_asci(ord(list(set(lines[i]) & set(lines[i+1]) & set(lines[i+2]))[0]))
+    return part_two_score
 
 def main():
     # Init
@@ -28,18 +28,13 @@ def main():
     file = open(file_path, "r")
     lines = file.readlines()
 
-    
     # Part 1
-    scores = [part_one(line) for line in lines]
-    part_one_score = sum(scores)
+    part_one_score = sum([part_one(line) for line in lines])
 
     # Part 2
-    part_two_score = 0
-    for i in range(0, len(lines), 3):
-        letter = list(set(lines[i]) & set(lines[i+1]) & set(lines[i+2]))[0]
-        letter_asci = ord(letter)
-        part_two_score += score_asci(letter_asci)
+    part_two_score = part_two(lines)
 
+    # Output
     print(f"Part 1: {part_one_score}")
     print(f"Part 2: {part_two_score}")
     
