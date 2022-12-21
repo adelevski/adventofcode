644d0edc7e5dd623b34b92da168840b295f65c58
Atanas Delevski <adelevski@gmail.com>
2022-12-21 15:28:34 -0600

    started day5 scala

--- src/main/scala/day5.scala
+++ src/main/scala/day5.scala
@@ -2,7 +2,11 @@ import scala.io.Source
 
 
 @main def day5() = {
-    val lines: List[String] = Source.fromFile("data/day5.txt").getLines.toList
-    println(lines)
+    val fn: String = "data/smol.txt"
+    val tb: Array[String] = Source.fromFile(fn).mkString.split("\n\n")
+    val a = tb(0).split("\n").reverse.toList.tail.map(x => x.tail.grouped(4).map(_.head).toList)
+    println(a)
+    // val lines: List[String] = Source.fromFile(fn).getLines.toList
+    // println(lines)
 }
 
